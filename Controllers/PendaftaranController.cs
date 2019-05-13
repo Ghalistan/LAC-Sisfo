c101c154bd5b97effd4bef4ce6d2911bbacdb5d3
Ghalistan <gghalistan@gmail.com>
2019-05-13 17:08:45 +0700

    More Pages

--- Controllers/PendaftaranController.cs
+++ Controllers/PendaftaranController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http;
+using LaC.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LaC.Controllers
@@ -11,6 +15,18 @@ namespace LaC.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult DataPendaftaran()
+        {
+            ViewBag.data = testAsync().Result;
+            return View();
+        }
+
+        public async Task<List<HistoryModel>> testAsync()
+        {
+            var getresp = await "http://localhost:8000/api/v1/lac/history/getAll".GetJsonAsync<List<HistoryModel>>();
+            return getresp;
         }
     }
 }
